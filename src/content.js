3b43121915e14f239cd1196e5f1fa11c8978bd37
Max Harney <mharney@moneygram.com>
2019-03-28 15:40:27 -0500

    Cleaning up displayToasty to not create multiple toasty guys

--- src/content.js
+++ src/content.js
@@ -1,25 +1,34 @@
 // injectScript('https://adrianotiger.github.io/web-esheep/src/esheep.js');
 
-(function() {
-    chrome.runtime.onMessage.addListener(receivefunc);
-    function receivefunc(msg,sender,sendResponse){
-        if(msg.txt === "toasty") {
-            displayToasty();
-        }
+(function () {
+
+  var toastyGuyElement, toastyAudioElement;
+
+  window.onload = function () {
+    document.body.innerHTML += '<div id="toasty-guy-dan"><img src="' + chrome.extension.getURL('toasty.png') + '" alt="toasty"></div>';
+    document.body.innerHTML += '<audio id="toasty-audio"><source src="' + chrome.extension.getURL('toasty.mp3') + '" type="audio/mpeg"></source></audio>';
+
+    toastyGuyElement = document.getElementById('toasty-guy-dan');
+    toastyAudioElement = document.getElementById('toasty-audio');
+  }
+
+  chrome.runtime.onMessage.addListener(receivefunc);
+  function receivefunc(msg, sender, sendResponse) {
+    if (msg.txt === "toasty") {
+      displayToasty();
+    }
+  }
+
+  function displayToasty() {
+    if (!toastyGuyElement || !toastyAudioElement) {
+      return;
     }
 
-    function displayToasty() {
-      //Add to dom needed elements
-      document.body.innerHTML += '<div id="toasty-guy-dan"><img src="'+ chrome.extension.getURL('toasty.png') +'" alt="toasty"></div>';
-      document.body.innerHTML += '<audio id="toasty-audio"><source src="'+ chrome.extension.getURL('toasty.mp3') +'" type="audio/mpeg"></source></audio>';
+    toastyAudioElement.play();
 
-      var toastyGuy = document.getElementById('toasty-guy-dan');
-      var toastyAudio = document.getElementById('toasty-audio');
-      toastyAudio.play();
-
-      toastyGuy.classList.add("show-dan");
-      setTimeout(function(){ 
-        toastyGuy.classList.remove("show-dan"); 
-      }, 1000);
-    }
+    toastyGuyElement.classList.add("show-dan");
+    setTimeout(function () {
+      toastyGuyElement.classList.remove("show-dan");
+    }, 1000);
+  }
 }());
