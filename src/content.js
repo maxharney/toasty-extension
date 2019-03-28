419bb1f35308ceebb2a3569f26cbb13ad7499192
Max Harney <mharney@moneygram.com>
2019-03-28 15:35:16 -0500

    Cleaning up toasty js and removing dependency on jquery

--- src/content.js
+++ src/content.js
@@ -9,7 +9,17 @@
     }
 
     function displayToasty() {
-      $("body").toasty();
-      $("body").toasty('pop');
+      //Add to dom needed elements
+      document.body.innerHTML += '<div id="toasty-guy-dan"><img src="'+ chrome.extension.getURL('toasty.png') +'" alt="toasty"></div>';
+      document.body.innerHTML += '<audio id="toasty-audio"><source src="'+ chrome.extension.getURL('toasty.mp3') +'" type="audio/mpeg"></source></audio>';
+
+      var toastyGuy = document.getElementById('toasty-guy-dan');
+      var toastyAudio = document.getElementById('toasty-audio');
+      toastyAudio.play();
+
+      toastyGuy.classList.add("show-dan");
+      setTimeout(function(){ 
+        toastyGuy.classList.remove("show-dan"); 
+      }, 1000);
     }
 }());
