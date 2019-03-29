22c308a2917ad6659f172919b86db673110475ce
Max Harney <mharney@moneygram.com>
2019-03-29 07:59:19 -0500

    Cleaning up toasty.js to fix issue with toasty guy sometimes not appearing

--- src/toasty.js
+++ src/toasty.js
@@ -1,23 +1,74 @@
 // injectScript('https://adrianotiger.github.io/web-esheep/src/esheep.js');
 
 (function () {
+  var toastyGuyElement = getToastyGuy();
+  var toastyAudioElement = getToastyAudio();
 
-  var toastyGuyElement = document.getElementById('toasty-guy-dan');
-  var toastyAudioElement = document.getElementById('toasty-audio');
-
-  if (!toastyGuyElement) {
-    document.body.innerHTML += '<div id="toasty-guy-dan"><img src="' + chrome.extension.getURL('images/toasty.png') + '" alt="toasty"></div>';
-    toastyGuyElement = document.getElementById('toasty-guy-dan');
-  }
-
-  if (!toastyAudioElement) {
-    document.body.innerHTML += '<audio id="toasty-audio"><source src="' + chrome.extension.getURL('sounds/toasty.mp3') + '" type="audio/mpeg"></source></audio>';
-    toastyAudioElement = document.getElementById('toasty-audio');
-  }
-
-  toastyAudioElement.play();
   toastyGuyElement.classList.add("show-dan");
+  toastyAudioElement.play();
+
   setTimeout(function () {
     toastyGuyElement.classList.remove("show-dan");
   }, 1000);
+
+  /**
+   * Retrieve the existing 'toasty' image or create a new one and append to the document body
+   */
+  function getToastyGuy() {
+    var toastyGuyId = 'toasty-guy-dan';
+    var toastyGuyElement = document.getElementById(toastyGuyId);
+
+    if (toastyGuyElement) {
+      return toastyGuyElement;
+    }
+
+    toastyGuyElement = createElement('div', { id: toastyGuyId });
+    toastyGuyImage = createElement('img', { src: chrome.extension.getURL('images/toasty.png') });
+
+    toastyGuyElement.appendChild(toastyGuyImage);
+    document.body.appendChild(toastyGuyElement);
+
+    return toastyGuyElement;
+  }
+
+  /**
+   * Retrieve the existing 'toasty' audio or create a new one and append to the document body
+   */
+  function getToastyAudio() {
+    var toastyAudioId = 'toasty-audio';
+    var toastyAudioElement = document.getElementById(toastyAudioId);
+
+    if (toastyAudioElement) {
+      return toastyAudioElement;   
+    }
+    
+    toastyAudioElement = createElement('audio', { id: toastyAudioId });
+    toastyAudioSource = createElement('source', { 
+      src: chrome.extension.getURL('sounds/toasty.mp3'),
+      type: 'audio/mpeg'
+    });
+
+    toastyAudioElement.appendChild(toastyAudioSource);
+    document.body.appendChild(toastyAudioElement);
+
+    return toastyAudioElement;
+  }
+
+  function createElement(elementType, options) {
+    var element = document.createElement(elementType);
+
+    if (options && options.id) {
+      element.setAttribute('id', options.id);
+    }
+
+    if (options && options.src) {
+      element.setAttribute('src', options.src);
+    }
+
+    if (options && options.type) {
+      element.setAttribute('type', options.type);
+    }
+
+    return element;
+  }
 }());
